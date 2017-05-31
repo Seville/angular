c665e4024774e524965886506ae1509549bba75f
Seville Worrell <Seville Worrell>
2017-05-30 22:18:48 -0400

    student service, model, data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpModule, JsonpModule } from '@angular/http';
+
 
 import { AppComponent }  from './app.component';
-import { StudentFormComponent }  from './student/components/student-form.component';
+import { StudentFormComponent }  from './student/component/student-form.component';
+import { StudentService }  from './student/component/services/student.service';
+
 
 @NgModule({
   imports: [
 	BrowserModule,
-	FormsModule
+	FormsModule,
+	HttpModule,
+	JsonpModule
   ],
   declarations: [
 	AppComponent,
 	StudentFormComponent
   ],
-  bootstrap:    [ AppComponent ]
+  providers: [StudentService],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
